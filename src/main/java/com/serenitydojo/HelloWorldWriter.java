6240be687966756adf9fb87e5d3ccf7704de1dfc
Naga Gorla <181894943+Naga67@users.noreply.github.com>
2024-10-22 10:24:21 +1300

    New msg

--- src/main/java/com/serenitydojo/HelloWorldWriter.java
+++ src/main/java/com/serenitydojo/HelloWorldWriter.java
@@ -3,6 +3,6 @@ package com.serenitydojo;
 public class HelloWorldWriter {
 
     public void writeHelloWorld() {
-        System.out.println("Hello world!");
+        System.out.println("Hello World! From Naga");
     }
 }
